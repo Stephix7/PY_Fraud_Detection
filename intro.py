4714d0743130bdd3dbc67c156cbf542fdd68e7ec
Stefan Jaspers <Stephix7@users.noreply.github.com>
2018-12-09 02:27:15 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,2 +1,97 @@
 # Examining the shape of the data set
 df = pd.read.csv('CC_card.csv')
+df.head()
+df.shape
+##
+# Once the labeled fraud file is read in 
+
+# Import pandas and read csv
+import pandas as pd
+df = pd.read_csv("creditcard_data.csv")
+
+# Explore the features available in your dataframe
+print(df.info())
+
+# Count the occurrences of fraud and no fraud and print them
+occ = df['Class'].value_counts()
+print(occ)
+
+# Print the ratio of fraud cases
+print(occ / len(df.index))
+
+# -------------------------------------------------------------
+# Prep data
+
+def prep_data(df):
+    X = df.iloc[:, 1:29]
+    X = np.array(X).astype(np.float)
+    y = df.iloc[:, 29]
+    y=np.array(y).astype(np.float)
+    return X,y
+
+
+
+# -------------------------------------------------------------
+# Plotting the data 
+
+# Define a function to create a scatter plot of our data and labels
+import 
+def plot_data(X, y):
+	plt.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
+	plt.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='y')
+	plt.legend()
+	return plt.show()
+
+# Create X and y from the prep_data function 
+X, y = prep_data(df)
+
+# Plot our data by running our plot data function on X and y
+plot_data(X, y)
+
+# ---------------------------------------------------------------
+# Applying oversampling to bring 1% of the fraud cases to 50 % 
+# technique used : SMOTE / Synthetic Minority Over Sampling Technique
+
+from imblearn.over_sampling import SMOTE
+
+# Run the prep_data function
+X, y = prep_data(df)
+
+# Define the resampling method
+method = SMOTE(kind='regular')
+
+# Create the resampled feature set
+X_resampled, y_resampled = method.fit_sample(X, y)
+
+# Plot the resampled data
+plot_data(X_resampled, y_resampled)
+
+# ---------------------------------------------------------------
+# Compare Original Data vs SMOTE Data generated 
+# technique used : SMOTE / Synthetic Minority Over Sampling Technique
+
+# source code - compare_plot
+
+def compare_plot(X,y,X_resampled,y_resampled, method):
+    # Start a plot figure
+    f, (ax1, ax2) = plt.subplots(1, 2)
+    # sub-plot number 1, this is our normal data
+    c0 = ax1.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0",alpha=0.5)
+    c1 = ax1.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1",alpha=0.5, c='r')
+    ax1.set_title('Original set')
+    # sub-plot number 2, this is our oversampled data
+    ax2.scatter(X_resampled[y_resampled == 0, 0], X_resampled[y_resampled == 0, 1], label="Class #0", alpha=.5)
+    ax2.scatter(X_resampled[y_resampled == 1, 0], X_resampled[y_resampled == 1, 1], label="Class #1", alpha=.5,c='r')
+    ax2.set_title(method)
+    # some settings and ready to go
+    plt.figlegend((c0, c1), ('Class #0', 'Class #1'), loc='lower center',
+                  ncol=2, labelspacing=0.)
+    #plt.tight_layout(pad=3)
+    return plt.show()
+  
+ 
+
+
+
+
+
